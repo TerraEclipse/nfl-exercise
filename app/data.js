81a8f97e9fcc826185432552496cbcc336269ced
Brian Link <cpsubrian@gmail.com>
2013-09-04 19:39:50 -0700

    Adds basic app structure.

---
+++ app/data.js
@@ -0,0 +1,5 @@
+var players = require('../data/Player.2012.json')
+  , teams = require('../data/Team.2012.json')
+  , data = module.exports = {};
+
+
--- app/server.js
+++ app/server.js
@@ -1,9 +1,29 @@
-/**
- *
- *
- *
- *  [Your Awesome App Here]
- *
- *
- *
- */
+var middler = require('middler')
+  , buffet = require('buffet')({root: './public'})
+  , templ = require('templ')('./views')
+  , server = require('http').createServer()
+  , router = middler(server)
+  , data = require('./data');
+
+// Add buffet middleware.
+router.add(-1000, buffet);
+router.add(1000, buffet.notFound);
+
+// Add templ middleware.
+router.add(-900, templ);
+
+router.get('/', function (req, res, next) {
+  res.render('index');
+});
+
+router.get('/teams', function (req, res, next) {
+  res.render('teams');
+});
+
+router.get('/players', function (req, res, next) {
+  res.render('players');
+});
+
+server.listen(3000, function () {
+  console.log('Started server on port ' + 3000);
+});
